aed6ff699c3f2cd03b6dde4b9fd55fa9b9105090
1 <xartesx@ukr.net>
2017-09-25 22:45:23 +0300

    123

--- Memory Training/MainActivity.cs
+++ Memory Training/MainActivity.cs
@@ -1,10 +1,11 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
-using System;
-using System.Timers;
 using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
 using System.Threading;
+using System.Timers;
 
 namespace Memory_Training
 {
@@ -12,20 +13,38 @@ namespace Memory_Training
     public class MainActivity : Activity
     {
        // LinearLayout _mainPage;
-        Button _button1;//, button2;
+        Button _button1;
         GridLayout _cardsField;
         System.Timers.Timer timer1;
-       // List<Point> _cards_Point = new List<Point>();
+        Spinner _spinner;
+        List<int> _numberForCardImage = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        int rowCount;
+        int columnCount;
+
+        ImageButton Card;
+        ImageButton _firstCard;
+        ImageButton _secondCard;
+        int step = 10;
+
+       
         protected override void OnCreate(Bundle savedInstanceState)
         {
             RequestWindowFeature(WindowFeatures.NoTitle);
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-           // _mainPage = FindViewById<LinearLayout>(Resource.Id.mainPage1);
+            List<string> stringForSpinner = new List<string>();
+            stringForSpinner.Add("4x3");
+            stringForSpinner.Add("4x4");
+            _spinner = FindViewById<Spinner>(Resource.Id.spinner1);
+            _spinner.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, stringForSpinner);
+            
+            
+            // _mainPage = FindViewById<LinearLayout>(Resource.Id.mainPage1);
             _button1 = FindViewById<Button>(Resource.Id.button1);
             _cardsField = FindViewById<GridLayout>(Resource.Id.gridLayout1);
-             
+            //ImageButton imageButton = FindViewById<ImageButton>(Resource.Id.imageButton1);
             _cardsField.RowCount = 4;
             _cardsField.ColumnCount = 4;
             _button1.Click += button1_Click;
@@ -36,31 +55,42 @@ namespace Memory_Training
 
         private void Timer1_Elapsed(object sender, ElapsedEventArgs e)
         {
-            MoveCards();
-
+            MoveCards(); 
             var needStopTimer = (_secondCard == null && _firstCard.RotationY == 180)
                                 || _secondCard.RotationY == 0;
+
             if (needStopTimer)
                 timer1.Stop();
 
             InvertStep();
-
             //если открыли вторую карту, даем 2 секунды на посмотреть
             if (_secondCard?.RotationY == 180)
             {
                 timer1.Stop();
+                if (_firstCard.Tag.ToString() == _secondCard.Tag.ToString())
+                {
+                    _secondCard.Dispose();
+                    _firstCard.Dispose();
+                    //_secondCard.Enabled = false;
+                    //_firstCard.Enabled = false;
+                    ResetCards();
+                    step = 10;
+                    return;
+                }
                 Thread.Sleep(TimeSpan.FromSeconds(1));
                 timer1.Start();
+                
                 //TODO:
                 //либо закрыть карты если разные
                 //TODO:доделать заполнение переменной проверкой одинаковые ли картинки
-                var sameImages = false;
-                if (sameImages)
-                {
-                    _firstCard.Visibility = _secondCard.Visibility = ViewStates.Gone;
-                    ResetCards();
-                    return;
-                }
+                //var sameImages = false;
+               // if (sameImages)
+                //{
+                    
+                   // _firstCard.Visibility = _secondCard.Visibility = ViewStates.Gone;
+                  //  ResetCards();
+                  //  return;
+               // }
             }
 
             if (_secondCard != null && _secondCard.RotationY == 0)
@@ -69,43 +99,72 @@ namespace Memory_Training
             }
         }
 
-        Button Card;
-        
-        int i = 0;
+        private int generateNumForCardTag()
+        {
+            Random number = new Random();
+            int num = number.Next(1, 9);
+            if (_numberForCardImage.Contains(num))
+            {
+                Card.Tag = num;
+                _numberForCardImage.Remove(num);
+                return num;
+            }
+            return generateNumForCardTag();
+        }
+
+
         void generateCards()
         {
-            Card = new Button(this);
-            Card.Click += card_Click;
-            Card.Text = 5.ToString();
-            _cardsField.AddView(Card);
-            Card.LayoutParameters.Width = 300;
-            Card.LayoutParameters.Height = 300;
-        }
-        void button1_Click(object Cards, EventArgs e)
-        {
-            i++;
-            for (int j=0; j<4; j++)
+            
+            for (int row = 0; row < rowCount; row++)
             {
-                for(int k=0;k<4;k++)
+                for (int colum = 0; colum < columnCount; colum++)
                 {
-                    generateCards();
+                    cardParameters();
                 }
             }
         }
-        Button _firstCard;
-        Button _secondCard;
-        bool _isFirstCardOpen = false;
-        bool _isSecondCardOpen = false;
-        int step = 10;
+
+        void cardParameters()
+        {
+            Card = new ImageButton(this);
+            Card.SetBackgroundResource(Resource.Drawable.quest);
+            Card.Click += card_Click;
+            generateNumForCardTag();
+            _cardsField.AddView(Card);
+            Card.LayoutParameters.Width = 100;
+            Card.LayoutParameters.Height = 100;
+        }
+ 
+        void button1_Click(object sender, EventArgs e)
+        {
+            var item = _spinner.SelectedItem.ToString();
+            if (item == "4x3")
+            {
+                columnCount = 3;
+                rowCount = 4;
+            }
+            if (item == "4x4")
+            {
+                columnCount = 4;
+                rowCount = 4;
+            }
+
+            generateCards();
+
+        }
+        
+        
         void card_Click(object sender, EventArgs e)
         {
-            if(timer1.Enabled)
+            if (timer1.Enabled)
                 return;
 
             if (_firstCard == null)
-                _firstCard = (Button)sender;
-            else if (_secondCard == null)
-                _secondCard = (Button)sender;
+                _firstCard = (ImageButton)sender;
+            else if (_secondCard == null && sender != _firstCard)
+                _secondCard = (ImageButton)sender;
+
             else return;
 
             StartMoveCards();
@@ -120,30 +179,41 @@ namespace Memory_Training
         {
             if (_secondCard != null && _secondCard.RotationY == 180 || _secondCard != null && _secondCard.RotationY == 0)
                 step *= -1;
-
-            
         }
 
         private void MoveCards()
         {
             if (_secondCard != null && step < 0)
             {
+                if (_firstCard.RotationY == 90 && _secondCard.RotationY == 90)
+                {
+                    _firstCard.SetBackgroundResource(Resource.Drawable.quest); 
+                    _secondCard.SetBackgroundResource(Resource.Drawable.quest); 
+                }
                 _secondCard.RotationY += step;
                 _firstCard.RotationY += step;
                 return;
             }
+
             if (_secondCard != null)
+            {
                 _secondCard.RotationY += step;
+                if (_secondCard.RotationY == 90)
+                    _secondCard.SetBackgroundResource(Resource.Drawable.animal_card1);
+            }
             else
+            {
                 _firstCard.RotationY += step;
-        }
-        
+                if (_firstCard.RotationY == 90)
+                    _firstCard.SetBackgroundResource(Resource.Drawable.animal_card1);
+            }
+         }
+
         private void ResetCards()
         {
             _secondCard = null;
             _firstCard = null;
         }
- 
     }
 }
  
--- Memory Training/Resources/Resource.Designer.cs
+++ Memory Training/Resources/Resource.Designer.cs
@@ -45,10 +45,34 @@ namespace Memory_Training
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int sample_2 = 2130837504;
+			public const int animal_card1 = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int sample_3 = 2130837505;
+			public const int animal_card2 = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int animal_card3 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int animal_card4 = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int animal_card5 = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int animal_card6 = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int animal_card7 = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int animal_card8 = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int animal_card9 = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int quest = 2130837513;
 			
 			static Drawable()
 			{
@@ -71,6 +95,9 @@ namespace Memory_Training
 			
 			// aapt resource value: 0x7f050000
 			public const int mainContainer = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int spinner1 = 2131034115;
 			
 			static Id()
 			{
